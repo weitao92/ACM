4eadf58e71fbbc9ca040daef257e540eb866cf41
weitao92 <weitao92@vt.edu>
2018-03-20 17:29:23 +0800

    dp

--- topics/DynamicProgramming/E.py
+++ topics/DynamicProgramming/E.py
@@ -1,1 +1,12 @@
 #https://pcs.cs.cloud.vt.edu/problems/223
+from bisect import bisect_left
+n = int(input())
+initial = list(map(int, input().split()))
+dp = []
+for i in range(n):
+    pos = bisect_left(dp,initial[i])
+    if pos >= len(dp):
+        dp.append(initial[i])
+    else:
+        dp[pos] = initial[i]
+print(len(dp)) 
